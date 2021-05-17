de560d9d2605aa10e958fc7611ceeaef48372850
satria1697 <andhikasatriab@gmail.com>
2021-05-17 22:41:52 +0700

    fixing css

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -22,30 +22,3 @@ export default {
   }
 }
 </script>
-
-<style>
-@import "assets/css/main.css";
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-  Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-/* Chrome, Safari, Edge, Opera */
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-/* Firefox */
-input[type='number'] {
-  -moz-appearance: textfield;
-}
-</style>
