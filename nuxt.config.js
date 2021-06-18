9dcf5b115fe63f4738277fdd759341d2024fdc8a
satria1697 <andhikasatriab@gmail.com>
2021-06-19 00:27:41 +0700

    implement nuxt config for deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   ],
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://api.globalindo-intl.com/api',
     post: {
       'content-type': 'application/x-www-form-urlencoded'
     },
