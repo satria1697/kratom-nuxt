01ad1eb4200fa39d3fd0b9795d4fcf168095cdd1
satria1697 <andhikasatriab@gmail.com>
2021-06-19 13:57:50 +0700

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,15 +62,6 @@ export default {
       ]
     }
   },
-  apps: [
-    {
-      name: 'NuxtAppName',
-      exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
-    }
-  ],
   env: {
     JWT_SECRET: 'bvMp8EzdcXZjUn0f5K3vOCblCL6xoRk4'
   },
