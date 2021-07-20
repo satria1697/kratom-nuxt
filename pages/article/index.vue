b3464a29112a5cddeaae946e728d62c5c16b1dec
aisyahmuhaddisi <aisyahmuhaddisi@mail.ugm.ac.id>
2021-07-21 06:59:37 +0700

    edited products, detail  and articles

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="container mx-auto">
-    <span class="text-4xl ml-3 lg:ml-0">{{ $t('article.articleTitle') }}</span>
+  <div class="container mx-auto pt-10 pb-10">
+    <h1 class="text-gray-800 text-center text-4xl font-light ml-3 lg:ml-0 text-center pb-4">{{ $t('article.articleTitle') }}</h1>
     <div
       v-for="(item, idx) in articles"
       :key="idx"
-      class="border-b border-main p-6 pb-7 mb-1.5 mx-4 lg:mx-0 cursor-pointer hover:shadow-lg transition-shadow flex flex-col lg:flex-row"
+      class="border-b border-main p-6 pb-7 mb-1.5 mx-4 lg:mx-0 cursor-pointer hover:shadow-lg transition-shadow grid grid-cols-5 lg:flex-row"
       @click="goTo('article-slug', {slug: item.slug})"
     >
-      <div class="w-full lg:w-[200px] mr-3">
+      <div class="w-full mr-3 col-span-2">
         <img alt="article-image" :src="item.image ? item.image : 'https://dummyimage.com/600x400/000/fff'">
       </div>
-      <div class="flex flex-col w-full">
+      <div class="flex flex-col w-full col-span-3 ml-3">
         <div class="flex flex-col lg:flex-row justify-between mb-2">
-          <span class="text-3xl font-semibold">
+          <span class="text-3xl font-light text-main tracking-wide">
             {{ langState === "en" ? item.title : item.title_id }}
           </span>
           <span class="text-gray-500 text-sm">{{ $t('article.createdAt') }} {{ convertTime(item.created_at) }}</span>
         </div>
-        <span class="mx-10 text-xl font-helvetica">{{ langState === "en" ? item.title : item.title_id }}</span>
+        <span class="text-md text-gray-700">{{ langState === "en" ? item.title : item.title_id }}</span>
+        <p id="detail" class="pt-3 text-gray-900 text-xl font-light">{{item.text}}</p>
       </div>
     </div>
   </div>
@@ -51,3 +52,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#detail {
+  overflow: hidden;
+  max-width: 25ch;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
--- pages/store/index.vue
+++ pages/store/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container w-screen mx-auto">
+  <div class="container w-screen mx-auto pt-10 pb-10">
     <select v-model="selectedCategory" class="rounded-md mb-3" @change="getGoods">
       <option v-for="(item, index) in category" :key="index" :value="item">
         {{ langState === "en" ? item.name : item.name_id }}
@@ -9,7 +9,7 @@
       <div
         v-for="(item, idx) in goods"
         :key="idx"
-        class="shadow-2xl mb-4 lg:mb-0 p-8 mx-auto w-3/4 lg:w-full flex flex-col rounded-lg bg-main hover:bg-main-light transition-colors"
+        class="mb-4 lg:mb-0 p-8 mx-auto w-3/4 lg:w-full flex flex-col bg-white hover:bg-main transition-colors group"
       >
         <div class="cursor-pointer" @click="goTo('detail-id', {id: item.id})">
           <img
@@ -19,8 +19,8 @@
             :alt="item.name"
           >
         </div>
-        <span class="mx-auto text-white text-2xl mt-auto">{{ langState === "en" ? item.name : item.name_id }}</span>
-        <span class="mx-auto font-semibold">USD {{ item.price }}</span>
+        <span class="mx-auto text-main text-2xl mt-auto tracking-wider font-light group-hover:text-white mt-3">{{ langState === "en" ? item.name : item.name_id }}</span>
+        <span class="mx-auto text-gray-400 font-thin tracking-widest group-hover:text-white">USD {{ item.price }}</span>
       </div>
     </div>
   </div>
