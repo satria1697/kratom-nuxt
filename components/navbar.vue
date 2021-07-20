94106a7ef7531cc07fc5d3642d68d36badfe3d5f
aisyahmuhaddisi <aisyahmuhaddisi@mail.ugm.ac.id>
2021-07-20 11:48:17 +0700

    edit home

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,63 +1,73 @@
 <template>
-  <div class="border-b-2 mb-3">
-    <div class="grid justify-items-stretch mt-3">
-      <div class="ml-2 cursor-pointer mr-5 justify-self-center" @click="goTo('index')">
-        <img class="w-44" :src="require('~/assets/jpg/logo.jpg')" alt="logo">
+  <div class="border-b-2">
+    <div class="container mx-auto">
+      <div class="grid grid-cols-3 justify-items-stretch mt-3 items-center">
+        <div></div>
+        <div class="ml-2 cursor-pointer mr-5 justify-self-center" @click="goTo('index')">
+          <img class="w-44" :src="require('~/assets/jpg/logo.jpg')" alt="logo">
+        </div>
+        <div class="mr-3 justify-self-end flex">
+          <div v-if="isLoading">
+            <span class="animate-pulse">Loading...</span>
+          </div>
+          <div v-else class="flex my-auto">
+            <div class="mr-3 lg:mr-6 cursor-pointer text-gray-500 hover:text-black" @click="goTo('cart')">
+              Cart
+              <!-- <img class="w-6 lg:w-8" :src="require('../assets/svg/shopping-cart.svg')"> -->
+            </div>
+            <div
+              v-if="jwt !== null && userInfo && userInfo.level >= 3"
+              class="mr-3 lg:mr-6 cursor-pointer text-gray-500 hover:text-black"
+              @click="goTo('profile')"
+            >
+              Profile
+              <!-- <img class="w-6 lg:w-8" :src="require('../assets/svg/profile-user.svg')"> -->
+            </div>
+            <div
+              v-if="jwt === null"
+              class="mr-3 lg:mr-6 cursor-pointer text-gray-500 hover:text-black"
+              @click="goTo('login')"
+            >
+              Login
+              <!-- <img class="w-6 lg:w-8" :src="require('../assets/svg/enter.svg')"> -->
+            </div>
+            <div
+              v-else
+              class="mr-3 lg:mr-6 cursor-pointer"
+              @click="handleLogout"
+            >
+              <img class="w-6 lg:w-8" :src="require('../assets/svg/logout.svg')">
+            </div>
+          </div>
+          <div class="mr-3">
+            <select class="py-1 px-8 rounded-md" @change="handleChangeLanguage">
+              <option v-for="(local, index) in availableLocales" :key="index" :value="local.code">
+                {{ local.name }}
+              </option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="container mx-auto flex h-[70px]">
-      <div class="my-auto flex items-center">
-        <div class="cursor-pointer hover:border-b-2 hover:border-main mr-3 hidden lg:block" @click="goTo('index')">
-          <span class="lg:text-xl font-semibold">Home</span>
+    <div class="container mx-auto flex mt-7">
+      <div class="my-auto flex items-center mx-auto">
+        <div class="cursor-pointer hover:border-b-2 hover:border-main hidden lg:block pb-4 px-5 text-gray-500 hover:text-black" @click="goTo('index')">
+          <span class="lg:text-xl font-light tracking-wider">Home</span>
         </div>
-        <div class="cursor-pointer hover:border-b-2 hover:border-main" @click="goTo('article')">
-          <span class="lg:text-xl font-semibold">Blog</span>
+        <div class="cursor-pointer hover:border-b-2 hover:border-main pb-4 px-5 text-gray-500 hover:text-black" @click="goTo('article')">
+          <span class="lg:text-xl font-light tracking-wider">Product</span>
         </div>
-      </div>
-
-      <div class="flex ml-auto my-auto">
-        <div class="mr-3">
-          <select class="py-1 px-8 rounded-md" @change="handleChangeLanguage">
-            <option v-for="(local, index) in availableLocales" :key="index" :value="local.code">
-              {{ local.name }}
-            </option>
-          </select>
+        <div class="cursor-pointer hover:border-b-2 hover:border-main hidden lg:block pb-4 px-5 text-gray-500 hover:text-black" @click="goTo('index')">
+          <span class="lg:text-xl font-light tracking-wider">News</span>
         </div>
-        <div v-if="isLoading">
-          <span class="animate-pulse">Loading...</span>
+        <div class="cursor-pointer hover:border-b-2 hover:border-main pb-4 px-5 text-gray-500 hover:text-black" @click="goTo('article')">
+          <span class="lg:text-xl font-light tracking-wider">About Us</span>
         </div>
-        <div v-else class="flex">
-          <div class="mr-3 lg:mr-6 cursor-pointer" @click="goTo('cart')">
-            <img class="w-6 lg:w-8" :src="require('../assets/svg/shopping-cart.svg')">
-          </div>
-          <div
-            v-if="jwt !== null && userInfo && userInfo.level < 3"
-            class="mr-3 lg:mr-6 cursor-pointer"
-            @click="goTo('admin-goods')"
-          >
-            <img class="w-6 lg:w-8" :src="require('../assets/svg/profile-user.svg')">
-          </div>
-          <div
-            v-else-if="jwt !== null && userInfo && userInfo.level >= 3"
-            class="mr-3 lg:mr-6 cursor-pointer"
-            @click="goTo('profile')"
-          >
-            <img class="w-6 lg:w-8" :src="require('../assets/svg/profile-user.svg')">
-          </div>
-          <div
-            v-if="jwt === null"
-            class="mr-3 lg:mr-6 cursor-pointer"
-            @click="goTo('login')"
-          >
-            <img class="w-6 lg:w-8" :src="require('../assets/svg/enter.svg')">
-          </div>
-          <div
-            v-else
-            class="mr-3 lg:mr-6 cursor-pointer"
-            @click="handleLogout"
-          >
-            <img class="w-6 lg:w-8" :src="require('../assets/svg/logout.svg')">
-          </div>
+        <div class="cursor-pointer hover:border-b-2 hover:border-main hidden lg:block pb-4 px-5 text-gray-500 hover:text-black" @click="goTo('index')">
+          <span class="lg:text-xl font-light tracking-wider">Contact</span>
+        </div>
+        <div class="cursor-pointer hover:border-b-2 hover:border-main pb-4 px-5 text-gray-500 hover:text-black" @click="goTo('article')">
+          <span class="lg:text-xl font-light tracking-wider">Faq</span>
         </div>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,125 @@
 <template>
-  <div class="container w-screen mx-auto">
-    <div class="text-center">
-      <img class="mx-auto mb-2 lg:w-1/4 w-2/3" alt="Logo-image" :src="require('../assets/jpg/logo.jpg')">
-    </div>
-    <select v-model="selectedCategory" class="rounded-md mb-3" @change="getGoods">
-      <option v-for="(item, index) in category" :key="index" :value="item">
-        {{ langState === "en" ? item.name : item.name_id }}
-      </option>
-    </select>
-    <div class="grid lg:grid-cols-4 lg:gap-10 lg:flex-row mx-auto w-full">
-      <div
-        v-for="(item, idx) in goods"
-        :key="idx"
-        class="shadow-2xl mb-4 lg:mb-0 p-8 mx-auto w-3/4 lg:w-full flex flex-col rounded-lg bg-main hover:bg-main-light transition-colors"
-      >
-        <div class="cursor-pointer" @click="goTo('detail-id', {id: item.id})">
-          <img
-            :src="
-              item.image ? item.image : 'https://dummyimage.com/600x400/000/fff'
-            "
-            :alt="item.name"
-          >
+  <div class="w-screen">
+    <!-- <div class="relative w-100">
+        <img class="block object-cover" src="../assets/jpg/image.jpeg" alt="Image" style="width:100%; height: 800px;">
+        <div class="absolute top-1/2 left-2/3 text-white">
+            <h1><span class="bg-black opacitiy-50 p-10"><b>BR</b></span> <span>Architects</span></h1>
         </div>
-        <span class="mx-auto text-white text-2xl mt-auto">{{ langState === "en" ? item.name : item.name_id }}</span>
-        <span class="mx-auto font-semibold">USD {{ item.price }}</span>
+    </div> -->
+    <div class="carousel relative shadow-xl bg-white">
+      <div class="carousel-inner relative overflow-hidden">
+        <!--Slide 1-->
+        <input class="carousel-open hidden" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
+        <div class="carousel-item absolute opacity-0" style="height:70vh;">
+          <div class="block h-full w-full text-center">
+            <img src="../assets/jpg/image.jpeg" class="object-contain" alt="Image">
+          </div>
+        </div>
+        <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+        <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+        
+        <!--Slide 2-->
+        <input class="carousel-open hidden" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
+        <div class="carousel-item absolute opacity-0" style="height:70vh;">
+          <div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">Slide 2</div>
+        </div>
+        <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+        <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label> 
+        
+        <!--Slide 3-->
+        <input class="carousel-open hidden" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
+        <div class="carousel-item absolute opacity-0" style="height:70vh;">
+          <div class="block h-full w-full bg-green-500 text-white text-5xl text-center">Slide 3</div>
+        </div>
+        <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+        <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+
+        <!-- Add additional indicators for each slide-->
+        <ol class="carousel-indicators">
+          <li class="inline-block mr-3">
+            <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+          </li>
+          <li class="inline-block mr-3">
+            <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+          </li>
+          <li class="inline-block mr-3">
+            <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+          </li>
+        </ol>
+        
       </div>
+    </div>
+    <div class="container mx-auto w-full mt-20">
+      <h1 class="text-gray-800 text-center text-4xl font-light mb-5">Produk</h1>
+      <select v-model="selectedCategory" class="rounded-md mb-3" @change="getGoods">
+        <option v-for="(item, index) in category" :key="index" :value="item">
+          {{ langState === "en" ? item.name : item.name_id }}
+        </option>
+      </select>
+      <div class="grid lg:grid-cols-4 lg:gap-10 lg:flex-row mx-auto w-full">
+        <div
+          v-for="(item, idx) in goods"
+          :key="idx"
+          class="mb-4 lg:mb-0 p-8 mx-auto w-3/4 lg:w-full flex flex-col bg-white hover:bg-main-light transition-colors group"
+        >
+          <div class="cursor-pointer" @click="goTo('detail-id', {id: item.id})">
+            <img
+              :src="
+                item.image ? item.image : 'https://dummyimage.com/600x400/000/fff'
+              "
+              :alt="item.name"
+            >
+          </div>
+          <span class="mx-auto text-main-dark text-2xl mt-auto tracking-wider font-light group-hover:text-white mt-3">{{ langState === "en" ? item.name : item.name_id }}</span>
+          <span class="mx-auto text-gray-400 font-thin tracking-widest group-hover:text-white">USD {{ item.price }}</span>
+        </div>
+      </div>
+      <section class="text-gray-700 body-font border-t border-gray-200">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-col text-center w-full mb-20">
+            <h1 class="sm:text-3xl text-4xl font-light title-font mb-4 text-gray-800">Our Team</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+          </div>
+          <div class="flex flex-wrap -m-2">
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="h-full items-center border-gray-200 border p-4 rounded-sm">
+                <img alt="team" class="w-1/2 bg-gray-100 object-cover object-center rounded-full mx-auto" src="https://dummyimage.com/80x80/edf2f7/a5afbd">
+                <div class="text-center mt-4">
+                  <h2 class="text-gray-900 title-font font-medium text-xl">Holden Caulfield</h2>
+                  <p class="text-gray-500">UI Designer</p>
+                </div>
+              </div>
+            </div>
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="h-full items-center border-gray-200 border p-4 rounded-sm">
+                <img alt="team" class="w-1/2 bg-gray-100 object-cover object-center rounded-full mx-auto" src="https://dummyimage.com/80x80/edf2f7/a5afbd">
+                <div class="text-center mt-4">
+                  <h2 class="text-gray-900 title-font font-medium text-xl">Holden Caulfield</h2>
+                  <p class="text-gray-500">UI Designer</p>
+                </div>
+              </div>
+            </div>
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="h-full items-center border-gray-200 border p-4 rounded-sm">
+                <img alt="team" class="w-1/2 bg-gray-100 object-cover object-center rounded-full mx-auto" src="https://dummyimage.com/80x80/edf2f7/a5afbd">
+                <div class="text-center mt-4">
+                  <h2 class="text-gray-900 title-font font-medium text-xl">Holden Caulfield</h2>
+                  <p class="text-gray-500">UI Designer</p>
+                </div>
+              </div>
+            </div>
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="h-full items-center border-gray-200 border p-4 rounded-sm">
+                <img alt="team" class="w-1/2 bg-gray-100 object-cover object-center rounded-full mx-auto" src="https://dummyimage.com/80x80/edf2f7/a5afbd">
+                <div class="text-center mt-4">
+                  <h2 class="text-gray-900 title-font font-medium text-xl">Holden Caulfield</h2>
+                  <p class="text-gray-500">UI Designer</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -98,3 +193,35 @@ export default {
   }
 }
 </script>
+
+<style>
+			.carousel-open:checked + .carousel-item {
+				position: static;
+				opacity: 100;
+			}
+			.carousel-item {
+				-webkit-transition: opacity 0.6s ease-out;
+				transition: opacity 0.6s ease-out;
+			}
+			#carousel-1:checked ~ .control-1,
+			#carousel-2:checked ~ .control-2,
+			#carousel-3:checked ~ .control-3 {
+				display: block;
+			}
+			.carousel-indicators {
+				list-style: none;
+				margin: 0;
+				padding: 0;
+				position: absolute;
+				bottom: 2%;
+				left: 0;
+				right: 0;
+				text-align: center;
+				z-index: 10;
+			}
+			#carousel-1:checked ~ .control-1 ~ .carousel-indicators li:nth-child(1) .carousel-bullet,
+			#carousel-2:checked ~ .control-2 ~ .carousel-indicators li:nth-child(2) .carousel-bullet,
+			#carousel-3:checked ~ .control-3 ~ .carousel-indicators li:nth-child(3) .carousel-bullet {
+				color: #2b6cb0;  /*Set to match the Tailwind colour you want the active one to be */
+			}
+		</style>
