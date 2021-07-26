02e158ecd5ff45b2a5dbc19b97f12d96c6d3dbdf
aisyahmuhaddisi <aisyahmuhaddisi@mail.ugm.ac.id>
2021-07-26 09:20:15 +0700

    added vue-carousel, error

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,15 @@
 <template>
   <div class="container 2xl:mx-auto">
     <div class="w-screen">
-      <div class="carousel relative shadow-xl bg-white">
-        <div class="carousel-inner relative overflow-hidden">
-          <!--Slide 1-->
-          <input
-            id="carousel-1"
-            class="carousel-open hidden"
-            type="radio"
-            name="carousel"
-            aria-hidden="true"
-            hidden=""
-            checked="checked"
-          >
-          <div class="carousel-item absolute opacity-0" style="height:70vh;">
-            <div class="block h-full w-full text-center">
-              <img src="../assets/jpg/image.jpeg" class="sm:object-contain object-cover" alt="Image">
-            </div>
-          </div>
-          <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-          <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-          <!--Slide 2-->
-          <input
-            id="carousel-2"
-            class="carousel-open hidden"
-            type="radio"
-            name="carousel"
-            aria-hidden="true"
-            hidden=""
-          >
-          <div class="carousel-item absolute opacity-0" style="height:70vh;">
-            <div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">
-              Slide 2
-            </div>
-          </div>
-          <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-          <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-          <!--Slide 3-->
-          <input
-            id="carousel-3"
-            class="carousel-open hidden"
-            type="radio"
-            name="carousel"
-            aria-hidden="true"
-            hidden=""
-          >
-          <div class="carousel-item absolute opacity-0" style="height:70vh;">
-            <div class="block h-full w-full bg-green-500 text-white text-5xl text-center">
-              Slide 3
-            </div>
-          </div>
-          <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-          <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-          <!-- Add additional indicators for each slide-->
-          <ol class="carousel-indicators">
-            <li class="inline-block mr-3">
-              <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-            </li>
-            <li class="inline-block mr-3">
-              <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-            </li>
-            <li class="inline-block mr-3">
-              <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-            </li>
-          </ol>
+      <div class="row">
+        <div class="col-sm mt-3 mt-sm-0">
+          <client-only>
+            <vue-carousel indicator-trigger="hover">
+              <div class="example-slide">Slide 1</div>
+              <div class="example-slide">Slide 1</div>
+              <div class="example-slide">Slide 1</div>
+            </vue-carousel>
+          </client-only>
         </div>
       </div>
     </div>
