1b494be2c26b1cd365aa9f3ac915a56c41c8b58e
andhika <andhikasatriab@gmail.com>
2021-07-27 19:03:26 +0700

    implement vue caroursel

--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,11 @@
 <template>
   <div class="container">
     <div class="w-screen">
-      <div class="carousel relative shadow-xl bg-white">
-        <div class="carousel-inner relative overflow-hidden">
-          <!--Slide 1-->
-          <input
-            id="carousel-1"
-            class="carousel-open hidden"
-            type="radio"
-            name="carousel"
-            aria-hidden="true"
-            hidden=""
-            checked="checked"
-          >
-          <div class="carousel-item absolute opacity-0" style="height:70vh;">
-            <div class="block h-full w-full text-center">
-              <img src="../assets/jpg/image.jpeg" class="sm:object-contain object-cover" alt="Image">
-            </div>
-          </div>
-          <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-          <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-          <!--Slide 2-->
-          <input
-            id="carousel-2"
-            class="carousel-open hidden"
-            type="radio"
-            name="carousel"
-            aria-hidden="true"
-            hidden=""
-          >
-          <div class="carousel-item absolute opacity-0" style="height:70vh;">
-            <div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">
-              Slide 2
-            </div>
-          </div>
-          <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-          <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-          <!--Slide 3-->
-          <input
-            id="carousel-3"
-            class="carousel-open hidden"
-            type="radio"
-            name="carousel"
-            aria-hidden="true"
-            hidden=""
-          >
-          <div class="carousel-item absolute opacity-0" style="height:70vh;">
-            <div class="block h-full w-full bg-green-500 text-white text-5xl text-center">
-              Slide 3
-            </div>
-          </div>
-          <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-          <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-          <!-- Add additional indicators for each slide-->
-          <ol class="carousel-indicators">
-            <li class="inline-block mr-3">
-              <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-            </li>
-            <li class="inline-block mr-3">
-              <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-            </li>
-            <li class="inline-block mr-3">
-              <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-            </li>
-          </ol>
+      <vue-slick-carousel v-bind="settings">
+        <div v-for="i in 3" :key="i" class="w-full h-[200px] xl:h-[400px]">
+          <img src="../assets/jpg/image.jpeg" class="sm:object-contain object-cover h-[800px] mx-auto w-full" alt="Image">
         </div>
-      </div>
+      </vue-slick-carousel>
     </div>
     <div class="w-screen 2xl:mx-auto">
       <div class="container mx-auto pt-20 pb-20">
@@ -210,7 +150,7 @@
 </template>
 
 <script>
-//import VueSlickCarousel from 'vue-slick-carousel'
+// import VueSlickCarousel from 'vue-slick-carousel'
 import KrtClickHereButton from '~/components/krt/simple/ClickHere'
 import KrtBox from '~/components/krt/simple/Box'
 import common from '~/mixin/common'
@@ -240,7 +180,17 @@ export default {
         name: 'All',
         name_id: 'Semua'
       }],
-      selectedCategory: null
+      selectedCategory: null,
+      settings: {
+        arrows: true,
+        dots: true,
+        infinite: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 10000,
+        cssEase: 'linear'
+      }
     }
   },
   head () {
